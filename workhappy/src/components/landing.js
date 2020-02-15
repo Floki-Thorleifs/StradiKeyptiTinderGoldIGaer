1e9756aa4952d942eeac3282c2ea52d5ab9543a7
Flow-flex-the-complex <fth29@hi.is>
2020-02-15 19:37:33 +0000

    temp

--- workhappy/src/components/landing.js
+++ workhappy/src/components/landing.js
@@ -1,15 +1,39 @@
 import React from 'react';
 
 export default function Landing() {
-
   return (
-
     <div className="App-body">
       <div className="box yellow">
-        <p>Halló hora</p>
+        <div className="yellow-content">
+          <p className="yellow-title">
+            Aukin ánægja starfsfólks fjarri höfuðstöðvunum
+          </p>
+          <p className="yellow-text">
+            Við gerum þér kleift að útvega starfsfólki sem frekar vilja vinna
+            nær sínu heimili vinnuaðstöðu í félagslegu umhverfi innan annarra
+            fyrirtækja.
+          </p>
+          <div className="yellow-buttons">
+            <a className="yellow-button" href="/">
+              Skrá mitt fyrirtæki
+            </a>
+            <a className="yellow-button" href="/">
+              Skrá mitt fyrirtæki
+            </a>
+          </div>
+          <div id="picture">
+            <p>Halló</p>
+          </div>
+        </div>
       </div>
       <div className="box white">
-        <p>Halló hora</p>
+        <div id="pictur2">
+          <p>Halló</p>
+        </div>
+        <p className="white-text">
+          Aukin afköst með nútímalegu starfsumhverfi og lausnum fyrir
+          sameiginleg vinnusvæði.
+        </p>
       </div>
       <div className="box pink">
         <p>Halló hora</p>
@@ -17,8 +41,6 @@ export default function Landing() {
       <div className="box end">
         <p>Halló hora</p>
       </div>
-
     </div>
-
-  )
-};
+  );
+}
