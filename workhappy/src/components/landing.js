f950c14813e37513241c7af39af11ca573576f90
Flow-flex-the-complex <fth29@hi.is>
2020-02-15 20:28:14 +0000

    temp2

--- workhappy/src/components/landing.js
+++ workhappy/src/components/landing.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Pic1 from '../img/026.png';
+import Pic2 from '../img/034.png';
+import Pic3 from '../img/024.png';
 
 export default function Landing() {
   return (
     <div className="App-body">
       <div className="box yellow">
         <div className="yellow-content">
-          <p className="yellow-title">
+          <p className="box-title">
             Aukin ánægja starfsfólks fjarri höfuðstöðvunum
           </p>
-          <p className="yellow-text">
+          <p className="box-text">
             Við gerum þér kleift að útvega starfsfólki sem frekar vilja vinna
             nær sínu heimili vinnuaðstöðu í félagslegu umhverfi innan annarra
             fyrirtækja.
@@ -21,22 +24,25 @@ export default function Landing() {
               Skrá mitt fyrirtæki
             </a>
           </div>
-          <div id="picture">
-            <p>Halló</p>
-          </div>
         </div>
+        <img className="box-picture" src={Pic1} alt="Woman" />
       </div>
       <div className="box white">
-        <div id="pictur2">
-          <p>Halló</p>
-        </div>
-        <p className="white-text">
+        <img className="box-picture" src={Pic2} alt="Woman" />
+        <p className="box-text center">
           Aukin afköst með nútímalegu starfsumhverfi og lausnum fyrir
           sameiginleg vinnusvæði.
         </p>
       </div>
       <div className="box pink">
-        <p>Halló hora</p>
+        <div className="box-content">
+          <p className="box-title">Ísland er meira en bara Reykjavík.</p>
+          <p className="box-text">
+            Víkkaðu sjóndeildarhringinn með félagslegri lausn á
+            vinnusvæða-hugtakinu.
+          </p>
+        </div>
+        <img className="box-picture" src={Pic3} alt="Woman" />
       </div>
       <div className="box end">
         <p>Halló hora</p>
