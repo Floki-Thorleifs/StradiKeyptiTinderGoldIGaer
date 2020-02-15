746f594a498290128184ad9bdd1546d334595f6d
Ásdís Erla <aej25@hi.is>
2020-02-15 20:39:26 +0000

    hover

--- workhappy/src/App.js
+++ workhappy/src/App.js
@@ -12,54 +12,6 @@ function App() {
       <Header />
       <Landing />
       <Footer />
-      {/* <div className="frame" id="n25_32">
-        <div className="left top component" id="n30_2">
-          <div className="scale rectangle" id="n25_95"></div>
-          <div className="scale text" id="n25_11">Aukin afköst með nútímalegu starfsumhverfi og lausnum fyrir sameiginleg vinnusvæði.</div>
-          <div className="scale rectangle" id="n25_13"></div>
-        </div>
-        <div className="left top component" id="n30_4">
-          <div className="scale rectangle" id="n25_96"></div>
-          <div className="left top rectangle" id="n25_17"></div>
-          <div className="left top text" id="n25_18">Skoða laus pláss</div>
-          <div className="scale rectangle" id="n25_19"></div>
-          <div className="scale text" id="n25_20">Skrá mitt fyrirtæki</div>
-          <div className="scale text" id="n25_23">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-        </div>
-        <div className="left top component" id="n30_3">
-          <div className="scale rectangle" id="n25_4"></div>
-          <div className="scale text" id="n25_21">Ísland er meira en bara Reykjavík.</div>
-          <div className="scale text" id="n25_22">Víkkaðu sjóndeildarhringinn með félagslegri lausn á vinnusvæða-hugtakinu.</div>
-          <div className="scale rectangle" id="n25_24"></div>
-        </div>
-        <div className="left top component" id="n30_5">
-          <div className="scale rectangle" id="n25_26"></div>
-          <div className="scale text" id="n25_27">Dunhagi 7, 102 Reykjavík
-		</div>
-          <div className="scale text" id="n25_28">8988733</div>
-        </div>
-        <div className="left top component" id="n30_1">
-          <div className="scale rectangle" id="n25_2"></div>
-          <div className="scale text" id="n25_5">Aukin ánægja starfsfólks fjarri höfuðstöðvum.</div>
-          <div className="scale text" id="n25_6">Við gerum þér kleift að útvega starfsfólki sem frekar vilja vinna nær sínu heimili vinnuaðstöðu í félagslegu umhverfi innan annarra fyrirtækja.</div>
-          <div className="scale rectangle" id="n25_12"></div>
-          <div className="scale rectangle" id="n25_15"></div>
-          <div className="scale text" id="n25_29">Skrá mitt fyrirtæki</div>
-          <div className="scale rectangle" id="n25_30"></div>
-
-          <div className="scale text" id="n25_31">Skoða laus pláss</div>
-        </div>
-        <div className="left top component" id="n30_0">
-          <div className="scale rectangle" id="n25_3"></div>
-          <div className="scale text" id="n25_7">Um okkur</div>
-          <div className="scale text" id="n25_8">Einstaklingar</div>
-          <div className="scale text" id="n25_9">Hafa samband</div>
-          <div className="scale text" id="n25_10">Fyrirtæki</div>
-          <div className="scale rectangle" id="n25_25"></div>
-          <div className="left top rectangle" id="n25_14"></div>
-          <div className="scale text" id="n25_16">Innskráning</div>
-        </div>
-      </div> */}
     </div>
   );
 }
