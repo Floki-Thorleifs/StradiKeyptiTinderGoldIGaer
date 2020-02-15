0066d755715dd21078decf2e8fadede9787e573b
Ásdís Erla <aej25@hi.is>
2020-02-15 19:20:23 +0000

    html og css bitch

---
+++ workhappy/src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../img/logo2.png';
+
+export default function Header() {
+
+  return (
+
+    <div className="App-header">
+      <div className="header logo">
+        <img src={Logo} className="logo" />
+      </div>
+      <nav className="navs">
+        <ul>
+          <li className="nav">Um okkur</li>
+          <li className="nav">Fyrirtæki</li>
+          <li className="nav">Einstaklingar</li>
+          <li className="nav">Hafa samband</li>
+          <li className="nav blue">Innskráning</li>
+        </ul>
+      </nav>
+      <p>Halló</p>
+    </div>
+  )
+};
---
+++ workhappy/src/components/landing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Landing() {
+
+  return (
+
+    <div className="App-body">
+      <div className="box yellow">
+        <p>Halló hora</p>
+      </div>
+      <div className="box white">
+        <p>Halló hora</p>
+      </div>
+      <div className="box pink">
+        <p>Halló hora</p>
+      </div>
+      <div className="box end">
+        <p>Halló hora</p>
+      </div>
+
+    </div>
+
+  )
+};
